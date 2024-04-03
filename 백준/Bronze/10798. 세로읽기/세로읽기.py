76a5013b99b9bd2f95940ba1556521132cfceb62
Minchae Kim <yjk199900@gmail.com>
2024-04-03 14:23:35 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -9,3 +9,11 @@ for i in range(maxLen):
     for j in range(5):
         if len(A[j]) > i:
             print(A[j][i], end='')
+
+# 다른 풀이
+# A = [input() for i in range(5)]
+
+# for j in range(15):
+#     for i in range(5):
+#         if j < len(A[i]):
+#             print(A[i][j], end='')
